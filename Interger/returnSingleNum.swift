86db78a5758a9185b34d5c0bd1d35cb0f51ae1fe
aabudu16 <52172492+aabudu16@users.noreply.github.com>
2020-04-12 09:11:52 -0400

    Create returnSingleNum.swift
    Create a function that returns the value from an array that only shows up ones.

---
+++ Interger/returnSingleNum.swift
@@ -0,0 +1,26 @@
+// Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ 
+// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ 
+ //input : [4,5,6,7,8,7,6,5,8]
+ //output: 4
+
+func singleOneInArray(arr:[Int])-> Int{
+    var dict:[Int:Int] = [:]
+    for i in arr{
+        if let value = dict[i]{
+        dict[i] = value + 1
+        } else {
+            dict[i] = 1
+        }
+    }
+    
+    for (key, val) in dict{
+        if val < 2 {
+        return key
+        }
+    }
+    return 0
+}
+
+singleOneInArray(arr: [4,5,6,7,8,7,6,5,8])
