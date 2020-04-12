b62f7712194037ca3c7c9ba49b18a2d6e7ee573b
aabudu16 <52172492+aabudu16@users.noreply.github.com>
2020-04-12 13:26:51 -0400

    Update removeDuplicates.swift

--- Linked List/removeDuplicates.swift
+++ Linked List/removeDuplicates.swift
@@ -8,6 +8,8 @@ This First approach grabs all the values from the LL recursively and inserts the
 Then we turn the set into an Array. looping through the Array, we remove the item from index 0 
 and creates a Node and asign that node to the previous Node while assigning the head 
 to the newly created Node
+
+// Since Set are not ordered, the linked list will not return in the same order
 */
 
 func removeDuplicates(linkL:LL?, set:Set<Int> = Set<Int>()) -> LL?{
@@ -36,3 +38,23 @@ func removeDuplicates(linkL:LL?, set:Set<Int> = Set<Int>()) -> LL?{
 }
 
 removeDuplicates(linkL: one)
+
+// Removing duplicates and keeing the same order.....
+
+func removeDuplicates(linkL: LL?) -> LL?{
+    let head = linkL
+    var current = linkL
+    var previous = linkL
+    var set = Set<Int>()
+    
+    while current != nil {
+        if !set.contains(current!.val) {
+            set.insert(current!.val)
+            previous = current
+        } else {
+            previous!.next = current!.next
+        }
+        current = current!.next
+    }
+    return head
+}
