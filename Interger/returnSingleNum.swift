b7e05c09cfb5e70e0185fb6215a97a6b94892d4a
aabudu16 <52172492+aabudu16@users.noreply.github.com>
2020-04-12 09:12:22 -0400

    Update returnSingleNum.swift
     Solving it without adding anything extra to memory except the dictionary

--- Interger/returnSingleNum.swift
+++ Interger/returnSingleNum.swift
@@ -24,3 +24,20 @@ func singleOneInArray(arr:[Int])-> Int{
 }
 
 singleOneInArray(arr: [4,5,6,7,8,7,6,5,8])
+
+// Solving it without adding anything extra to memory except the dictionary
+func singleOneInArray(arr:[Int])-> Int{
+    var dict:[Int:Int] = [:]
+    for i in arr{
+        dict = (dict[i] ?? 0) + 1
+    }
+    
+    for (key, val) in dict{
+        if val < 2 {
+        return key
+        }
+    }
+    return 0
+}
+
+singleOneInArray(arr: [4,5,6,7,8,7,6,5,8])
