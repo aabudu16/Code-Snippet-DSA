94f51b7e3e3d6fe893cfac9893254174bffc7b41
aabudu16 <52172492+aabudu16@users.noreply.github.com>
2020-04-13 16:08:26 -0400

    Update sumOfAllValues.swift

--- Stacks/sumOfAllValues.swift
+++ Stacks/sumOfAllValues.swift
@@ -1,1 +1,98 @@
-dvsd
+// Stack views
+// adds and removes stuff that is *on top of each other *
+// LIFO - last in, first out (the thing we just added will be what we remove)
+// At a given time, we're ONLY interacting with one element in the stack (the "top" of the stack)
+class Stack <T:Equatable>{
+    private var element = [T]()
+    // add things to the top
+    func push(value: T){
+        element.append(value)
+    }
+    // remove from the top
+    func pop() -> T?{
+        return element.popLast()
+    }
+
+    // look at the top
+    func peek() -> T?{
+        return element.last
+    }
+
+    // check if its empty and return bool
+    func isEmpty()-> Bool{
+        return element.isEmpty
+    }
+}
+
+// Given a stack of Int, return the sum of all items values in the stacl
+var stack = Stack<Int>()
+stack.push(value: 3)
+stack.push(value: 4)
+stack.push(value: 8)
+stack.push(value: 1)
+stack.push(value: 4)
+stack.push(value: 12)
+
+print(stack)
+
+func sumOfALL(stack:Stack<Int>)-> Int{
+    var total:Int = 0
+    var currentValue:Int!
+    while !stack.isEmpty(){
+        currentValue = stack.pop()
+        total += currentValue
+    }
+    return total
+}
+
+let sum = sumOfALL(stack: stack)
+print(sum)
+
+
+// Sum of all values in a stack recursively
+func sumOfAll(stack:Stack<Int>?, sum:Int = 0) -> Int?{
+    guard let stack = stack else {return nil}
+    var sum = sum
+    
+    while !stack.isEmpty() {
+        sum += stack.pop()!
+       return sumOfAll(stack: stack, sum: sum)
+    }
+   return sum
+}
+
+let sum = sumOfAll(stack: stack)
+print(sum)
+
+
+// Sum of all values in a stack using a closure recursively
+//1)
+func sumOfALL(stack:Stack<Int>?, closure:(Int)->Void){
+    guard let stack = stack else {return}
+    while !stack.isEmpty(){
+    closure(stack.pop()!)
+    return sumOfAlL(stack: stack, closure: closure)
+    }
+}
+
+var sum = 0
+sumOfAlL(stack: stack) { (stack) in
+    sum += stack
+}
+ print(sum)
+
+//2)
+//////////////////////////////////////////////////////////////////////////
+func sumOfAlL(stack:Stack<Int>?, closure:(Stack<Int>)->Void){
+    guard let stack = stack else {return}
+    while !stack.isEmpty(){
+    closure(stack)
+    return sumOfAlL(stack: stack, closure: closure)
+    }
+}
+
+var sum = 0
+sumOfAlL(stack: stack) { (stack) in
+    sum += stack.pop()!
+}
+ print(sum)
