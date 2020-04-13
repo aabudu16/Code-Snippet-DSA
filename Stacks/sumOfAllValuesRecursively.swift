ffb5aaa35e0d92e584a70ea715e5835e67519fee
aabudu16 <52172492+aabudu16@users.noreply.github.com>
2020-04-13 16:12:17 -0400

    Create sumOfAllValuesRecursively.swift

---
+++ Stacks/sumOfAllValuesRecursively.swift
@@ -0,0 +1,49 @@
+// Stack views
+// adds and removes stuff that is *on top of each other *
+// LIFO - last in, first out (the thing we just added will be what we remove)
+// At a given time, we're ONLY interacting with one element in the stack (the "top" of the stack)
+class Stack <T:Equatable>{
+    private var element = [T]()
+    // add things to the top
+    func push(value: T){
+        element.append(value)
+    }
+    // remove from the top
+    func pop() -> T?{
+        return element.popLast()
+    }
+
+    // look at the top
+    func peek() -> T?{
+        return element.last
+    }
+
+    // check if its empty and return bool
+    func isEmpty()-> Bool{
+        return element.isEmpty
+    }
+}
+
+// Given a stack of Int, return the sum of all items values in the stack recursively
+var stack = Stack<Int>()
+stack.push(value: 3)
+stack.push(value: 4)
+stack.push(value: 8)
+stack.push(value: 1)
+stack.push(value: 4)
+stack.push(value: 12)
+
+// Sum of all values in a stack recursively
+func sumOfAll(stack:Stack<Int>?, sum:Int = 0) -> Int?{
+    guard let stack = stack else {return nil}
+    var sum = sum
+    
+    while !stack.isEmpty() {
+        sum += stack.pop()!
+       return sumOfAll(stack: stack, sum: sum)
+    }
+   return sum
+}
+
+let sum = sumOfAll(stack: stack)
+print(sum)
