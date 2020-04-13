9941dbf14bc7b2a158a5cd51a40e4744cdfd5db2
aabudu16 <52172492+aabudu16@users.noreply.github.com>
2020-04-13 16:15:46 -0400

    Update sumOfAllValuesRecursively2.swift
    Implements a recursive function using a closure.

--- Stacks/sumOfAllValuesRecursively2.swift
+++ Stacks/sumOfAllValuesRecursively2.swift
@@ -1,1 +1,68 @@
-d
+// Stack views
+// adds and removes stuff that is *on top of each other *
+// LIFO - last in, first out (the thing we just added will be what we remove)
+// At a given time, we're ONLY interacting with one element in the stack (the "top" of the stack)
+class Stack <T:Equatable>{
+    private var element = [T]()
+    // add things to the top
+    func push(value: T){
+        element.append(value)
+    }
+    // remove from the top
+    func pop() -> T?{
+        return element.popLast()
+    }
+
+    // look at the top
+    func peek() -> T?{
+        return element.last
+    }
+
+    // check if its empty and return bool
+    func isEmpty()-> Bool{
+        return element.isEmpty
+    }
+}
+
+// Given a stack of Int, return the sum of all items values in the stack
+var stack = Stack<Int>()
+stack.push(value: 3)
+stack.push(value: 4)
+stack.push(value: 8)
+stack.push(value: 1)
+stack.push(value: 4)
+stack.push(value: 12)
+
+// Sum of all values in a stack using a closure recursively
+//1)
+func sumOfALL(stack:Stack<Int>?, closure:(Int)->Void){
+    guard let stack = stack else {return}
+    while !stack.isEmpty(){
+    closure(stack.pop()!)
+    return sumOfAlL(stack: stack, closure: closure)
+    }
+}
+
+var sum = 0
+sumOfAlL(stack: stack) { (stack) in
+    sum += stack
+}
+ print(sum)
+
+
+
+//2)
+//////////////////////////////////////////////////////////////////////////
+func sumOfAlL(stack:Stack<Int>?, closure:(Stack<Int>)->Void){
+    guard let stack = stack else {return}
+    while !stack.isEmpty(){
+    closure(stack)
+    return sumOfAlL(stack: stack, closure: closure)
+    }
+}
+
+var sum = 0
+sumOfAlL(stack: stack) { (stack) in
+    sum += stack.pop()!
+}
+ print(sum)
