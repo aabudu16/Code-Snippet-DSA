4c5ab361c1659afdee8e394eb45a8cb52bfcd748
aabudu16 <52172492+aabudu16@users.noreply.github.com>
2020-04-23 16:35:44 -0400

    Update moveZeroes.swift

--- Array/moveZeroes.swift
+++ Array/moveZeroes.swift
@@ -4,24 +4,22 @@
  Example:
  Input: [0,1,0,3,12]
  Output: [1,3,12,0,0]
- Note:
- You must do this in-place without making a copy of the array.
- Minimize the total number of operations.
  */
 
-func moveZeroes(_ nums: inout [Int]) {
-  var arr = [Int]()
-  var offset = 0
-    
-  for (i, v) in nums.enumerated() {
-    if v == 0 {
-      arr.append(i)
+ func moveZeroes(_ nums:[Int]) {
+        var arr = [Int]()
+        
+        for v in nums{
+            if v == 0 {
+                arr.append(v)
+            }
+        }
+        
+        for i in nums.reversed() {
+            if i != 0 {
+                arr.insert(i, at: 0)
+            }
+        }
+        
+        print(arr)
     }
-  }
-    
-  for i in arr {
-    nums.remove(at: i + offset)
-    offset -= 1
-    nums.append(0)
-  }  
-}
